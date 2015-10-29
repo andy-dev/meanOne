59dcac85479f5d55a1c6b36a1b8bf18a15a6e4f5
andy-dev <andres.macedo@gmail.com>
2015-10-29 14:42:31 -0700

    connected server to html file

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res){
-    res.send("hello from server")
-})
+// app.get('/', function(req, res){
+//     res.send("hello from server")
+// })
+
+app.use(express.static(__dirname + "/public"));
 
 app.listen(3000);
 console.log("server running in port 3000")
