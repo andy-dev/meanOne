8e61dd9ca9b2e2d49fc4cb8403d6b52610044e92
andy-dev <andres.macedo@gmail.com>
2015-10-29 16:26:13 -0700

    use scope and get to glue view and data

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,6 +1,29 @@
 var myApp = angular.module('myApp',[]);
 myApp.controller('AppCtrl',['$scope', '$http', function($scope,$http){
         console.log("hello world from contrl");
+
+        $http.get('/contactlist');
+
+        person1 ={
+            name:"andy",
+            email: "andy@gmail.com",
+            number: "9175431786",
+        };
+
+        person2 ={
+            name:"andy2",
+            email: "andy2@gmail.com",
+            number: "9175431786",
+        };
+
+        person3 ={
+            name:"andy3",
+            email: "andy3@gmail.com",
+            number: "9175431786",
+        };
+
+        var contactList = [person1,person2,person3];
+        $scope.contactList = contactList;
 }]);
 
 // function AppCtrl(){
--- server.js
+++ server.js
@@ -7,5 +7,9 @@ var app = express();
 
 app.use(express.static(__dirname + "/public"));
 
+app.get('/contactList', function(req, res){
+    console.log(" I received get req")
+})
+
 app.listen(3000);
 console.log("server running in port 3000")
